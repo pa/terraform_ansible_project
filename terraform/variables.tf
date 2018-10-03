2fa5ef5733636f3a7691e99f9618eb347455564a
pramodhayyappan92 <pramodh.ayyappan92@gmail.com>
2018-10-03 16:19:19 +0530

    updated ansible playbook

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_key_name" {}
 variable "aws_region" {
     description = "EC2 Region for the VPC"
     default = "us-east-1"
