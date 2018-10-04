9aad76b9c9b4189479e1751ddf99b0565722c33c
pramodhayyappan92 <pramodh.ayyappan92@gmail.com>
2018-10-04 17:03:27 +0530

    updated ansible playbook

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "state-storage-terraform"
-    key    = "terraform-execution-state/terraform_test.tfstate"
+    key    = "terraform-execution-state/"
     region = "us-east-1"
   }
 }
